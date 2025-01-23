d733c77d52c466a7eadf3441631dba24080074ac
RJ Trenchard <rjtrenchard@gmail.com>
2025-01-23 11:15:08 -0800

    add dependencies, add sample webpage

--- quiz-response.py
+++ quiz-response.py
@@ -2,9 +2,15 @@
 
 # initial commit.
 
+# pip install Flask
+# pip install groq
+
 # todo:
 # import flask
 # import AI API
 # serve a webpage
+
+from flask import Flask
+from groq import Groq
 
 print("Hello World")
